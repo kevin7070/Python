2601135b65ff4f3f75c8feb575916641acb6bfb4
kevin7070 <kevin.7070@gmail.com>
2021-07-05 11:21:23 +0800

    Update filter.py
    working path update

--- dump_jpg_from_WDT/filter.py
+++ dump_jpg_from_WDT/filter.py
@@ -7,7 +7,7 @@ sourceFolderName = 'HuaweiSDTData'
 # change sourceFolderName
 outPutFolderName = 'WDT COMM Photo'
 
-rootDir = 'C:\\Users\\Kevin\\Desktop\\test_folder\\' + sourceFolderName
+rootDir = 'C:\\Users\\Kevin\\Desktop\\dump_jpg_from_WDT\\' + sourceFolderName
 os.chdir(rootDir)
 print("Current working dir : %s" % os.getcwd())
 site_folders = os.listdir()
@@ -18,7 +18,7 @@ for index in range(len(site_folders)):
     print("Processing site folder: " + processing_folder)
 
     # make dir for new site_folder
-    new_folder = 'C:\\Users\\Kevin\\Desktop\\test_folder\\' + \
+    new_folder = 'C:\\Users\\Kevin\\Desktop\\dump_jpg_from_WDT\\' + \
         outPutFolderName + '\\' + site_folders[index]
     if not os.path.exists(new_folder):
         os.makedirs(new_folder)
